c139583ba9322bfbe77154a071da0e759b8e9d67
Nassimprog <56696078+Nassimprog@users.noreply.github.com>
2023-03-12 09:52:54 +0000

    Update index.js

--- 1/index.js
+++ 1/index.js
@@ -45,4 +45,6 @@ function sendEmail() {
   const mailtoLink = `mailto:${toEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}&from=${encodeURIComponent(email)}`;
 
   window.open(mailtoLink, '_blank');
+
+  alert("Email sent!");
 }
